5d79907964ed2435463c450d1227c8fee9fa7dc7
capotej <jcapote@gmail.com>
2018-02-06 17:37:04 -0500

    ignore unknown fields and null data for distribution types, add node distribution type

--- src/main/java/io/packagecloud/client/Distributions.java
+++ src/main/java/io/packagecloud/client/Distributions.java
@@ -1,14 +1,20 @@
 package io.packagecloud.client;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.List;
 
 /**
  * JSON class for Distributions Response
  */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Distributions {
     public List<Distribution> deb;
     public List<Distribution> dsc;
     public List<Distribution> rpm;
     public List<Distribution> py;
+    public List<Distribution> node;
     public List<Distribution> jar;
 }
