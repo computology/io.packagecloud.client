b01dca2f9737344718d3e4a581daf438ce8ecfe5
Julio Capote <jcapote@gmail.com>
2018-02-06 19:26:12 -0500

    Adds Node.js package support and fixes bug around parsing newly added distributions (#8)
    * add fixture of our distrubutions.json with a 'fake_unknown' distro added for testing
    
    * bump to 3.0.1
    
    * ignore unknown fields and null data for distribution types, add node distribution type
    
    * ignore any unknown fields for contents.json
    
    * add tgz extension to supportedExtensions

--- src/main/java/io/packagecloud/client/Contents.java
+++ src/main/java/io/packagecloud/client/Contents.java
@@ -1,10 +1,15 @@
 package io.packagecloud.client;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
 import java.util.List;
 
 /**
  * JSON class for Contents response
  */
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class Contents {
 
     public List<File> files;
